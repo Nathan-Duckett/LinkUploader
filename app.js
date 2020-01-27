4c7ea31d60292cbe2750563d54ad4dd115710561
Nathan Duckett <nduckett13@gmail.com>
2020-01-28 10:59:47 +1300

    Comment out error handling temporarily

--- app.js
+++ app.js
@@ -19,22 +19,22 @@ app.post("/add", (req, res) => {
   links.append(req.body.link);
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.send("error");
+// });
 
 app.listen(3000)

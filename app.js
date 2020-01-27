ff0abc581dd731a4c869969c3de5cfc904d54ea4
Nathan Duckett <nduckett13@gmail.com>
2020-01-28 11:11:25 +1300

    Add link management routes

--- app.js
+++ app.js
@@ -10,9 +10,31 @@ let links = []
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-        
+
+app.get("/get/:index", (req, res) => {
+  let index = req.params.index;
+  let link = links[index];
+  res.send(link);
+});
+
+app.get("/go:index", (req, res) => {
+  let index = req.params.index;
+  let link = links[index];
+  res.redirect(link);
+});
+
 app.get("/get", (req, res) => {
   res.send(links);
+});
+
+app.get("/clear", (req, res) => {
+  links = []
+  res.send("Cleared List");
+});
+
+app.get("/remove/:index", (req, res) => {
+  let index = req.params.index;
+  links.splice(index, 1);
 });
 
 app.post("/add", (req, res) => {
@@ -20,22 +42,22 @@ app.post("/add", (req, res) => {
   res.send("success");
 });
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.send("error");
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.send("error");
+});
 
 app.listen(3000)

89a70d96a953660c0417b8289b682ac1dc1ba823
Nathan Duckett <nduckett13@gmail.com>
2020-01-28 10:45:04 +1300

    Add routes for get links and add link

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+let links = []
+
 app.get("/", (req, res) => {
   res.send("Hello World");
-}
+});
+        
+app.get("/get", (req, res) => {
+  res.send(links);
+});
+
+app.post("/add", (req, res) => {
+  links.append(req.body.link);
+});
 
 app.listen(3000)
